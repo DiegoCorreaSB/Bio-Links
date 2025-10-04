2ae00c61b880020867ce87f8288f1557622909dc
Diego Corrêa <diegoberto2003@gmail.com>
2025-10-03 23:17:09 -0300

    feito o grosso

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,17 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
-
-
-
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+ 
 export const HomePage = () => {
 
 
     return (
-        <div className="main">
+      <div className="main">
+        <div className="container">
             <div id="profile">
-               <img src= {profileDiego} alt="Diego Corrêa" />
+               <img id="img-profile" src= {profileDiego} alt="Diego Corrêa" />
                <p>Diego Corrêa, 21 Anos</p>
             </div>
 
@@ -29,23 +28,25 @@ export const HomePage = () => {
                         <a href="#">Instagram</a>
                      </li>
                      <li>
-                        <a href="#">Twitter</a>
+                        <a href="#">Portfólio</a>
                      </li>
                   </ol>
             </div>
 
-            
-
             <div id="social-links">
-                  <a href="" className="iconsSocialLinks"><FontAwesomeIcon className="icon" icon={ faLinkedin } /></a>
-                  <a href="" className="iconsSocialLinks"><FontAwesomeIcon className="icon" icon={ faGithub } /></a>
-                  <a href="" className="iconsSocialLinks"><FontAwesomeIcon className="icon" icon={ faTiktok } /></a>
+                  <a href="" className="iconsSocialLinks"> <FontAwesomeIcon className="icon" icon={ faLinkedin } /> </a>
+                  <a href="https://github.com/DiegoCorreaSB" className="iconsSocialLinks" target="_blank"> <FontAwesomeIcon  className="icon" icon={ faGithub } /> </a>
+
+                  <a href="" className="iconsSocialLinks"> <FontAwesomeIcon className="icon" icon={ faTiktok } /> </a>
+
+                  <a href="https://www.instagram.com/dgcorrea__/" className="iconsSocialLinks" target="_blank"> <FontAwesomeIcon className="icon" icon={ faInstagram } /> </a>
             </div>
 
             <div id="footer">
                   <p>Feito por Diego Corrêa</p>
             </div>
         </div>
+      </div>
     )
  }
  
