b924890dbe6001397b8cf718aad6946e970ebf72
Diego Corrêa <diegoberto2003@gmail.com>
2025-10-05 10:45:32 -0300

    Criação do home css

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,49 +4,51 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
- 
+
 export const HomePage = () => {
 
 
-    return (
+   return (
       <div className="main">
-        <div className="container">
+         <div className="container">
             <div id="profile">
-               <img id="img-profile" src= {profileDiego} alt="Diego Corrêa" />
+               <img id="img-profile" src={profileDiego} alt="Diego Corrêa" />
                <p>Diego Corrêa, 21 Anos</p>
             </div>
 
             <div id="links">
-                  <ol>
-                     <li>
-                        <a href="#">Linkedin</a>
-                     </li>
-                     <li>
-                        <a href="#">GitHub</a>
-                     </li>
-                     <li>
-                        <a href="#">Instagram</a>
-                     </li>
-                     <li>
-                        <a href="#">Portfólio</a>
-                     </li>
-                  </ol>
+               <ol>
+                  <li>
+                     <a href="#">Portfólio</a>
+                  </li>
+                  <li>
+                     <a href="#">Calculator Project</a>
+                  </li>
+                  <li>
+                     <a href="#">Login Screen</a>
+                  </li>
+                  <li>
+                     <a href="https://diegocorreasb.github.io/orderTableJS/" target="_blank">Ordered Table</a>
+                  </li>
+                  <li>
+                     <a href="#">Sobre mim</a>
+                  </li>
+               </ol>
             </div>
 
             <div id="social-links">
-                  <a href="" className="iconsSocialLinks"> <FontAwesomeIcon className="icon" icon={ faLinkedin } /> </a>
-                  <a href="https://github.com/DiegoCorreaSB" className="iconsSocialLinks" target="_blank"> <FontAwesomeIcon  className="icon" icon={ faGithub } /> </a>
+               <a href="" className="iconsSocialLinks"> <FontAwesomeIcon className="icon" icon={faLinkedin} /> </a>
+               <a href="https://github.com/DiegoCorreaSB" className="iconsSocialLinks" target="_blank"> <FontAwesomeIcon className="icon" icon={faGithub} /> </a>
 
-                  <a href="" className="iconsSocialLinks"> <FontAwesomeIcon className="icon" icon={ faTiktok } /> </a>
+               <a href="" className="iconsSocialLinks"> <FontAwesomeIcon className="icon" icon={faTiktok} /> </a>
 
-                  <a href="https://www.instagram.com/dgcorrea__/" className="iconsSocialLinks" target="_blank"> <FontAwesomeIcon className="icon" icon={ faInstagram } /> </a>
+               <a href="https://www.instagram.com/dgcorrea__/" className="iconsSocialLinks" target="_blank"> <FontAwesomeIcon className="icon" icon={faInstagram} /> </a>
             </div>
 
             <div id="footer">
-                  <p>Feito por Diego Corrêa</p>
+               <p>Feito por Diego Corrêa</p>
             </div>
-        </div>
+         </div>
       </div>
-    )
- }
- 
+   )
+}
